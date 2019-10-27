e6dfde1e3f2d085c99bae04f81cc3d89b5178743
James Ah Yong <retrocraft314@gmail.com>
2019-10-27 12:40:12 -0400

    random fixes

--- bin/generateemoji.py
+++ bin/generateemoji.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
 
 import unicodedata
-import emoji
+import emoji_data_python
 import re
-from itertools import chain
 
-for e, a in chain(emoji.UNICODE_EMOJI.items(), emoji.UNICODE_EMOJI_ALIAS.items()):
+for key, value in emoji_data_python.emoji_short_names.items():
     try:
-        a = a.replace('_', '')
-        keys = [*a]
+        key = key.replace('_', '')
+        key = key.replace('-', '')
+        keys = [*key]
         comment = ""
-        print(comment, 'Mk {0} : "{1}" U{2} # {3}'.format(
-            ' '.join(keys), e, format(ord(e), 'x').upper(), unicodedata.name(e)))
+        print(comment, 'Mk Mk {0} : "{1}" U{2} # {3}'.format(
+            ' '.join(keys), emoji_data_python.unified_to_char(value.unified), format(value.unified).upper(), value.name))
     except Exception as e:
         continue
 
-for e, a in emoji.UNICODE_EMOJI_ALIAS.items():
-    try:
-        if (re.search('skin_tone', a) or re.search('[0-9]', a)):
-            continue
-        a = a.replace('_', '').replace(':', '')
-        keys = [*a]
-        if len(keys) <= 2 or len(keys) >= 7:
-            comment = "#"
-        else:
-            comment = ""
-        print(comment, 'Mk Mk {0} : "{1}" U{2} # {3}'.format(
-            ' '.join(keys), e, format(ord(e), 'x').upper(), unicodedata.name(e)))
-    except Exception as e:
-        continue
+# for e, a in emoji.UNICODE_EMOJI_ALIAS.items():
+#     try:
+#         if (re.search('skin_tone', a) or re.search('[0-9]', a)):
+#             continue
+#         a = a.replace('_', '').replace(':', '')
+#         keys = [*a]
+#         if len(keys) <= 2 or len(keys) >= 7:
+#             comment = "#"
+#         else:
+#             comment = ""
+#         print(comment, 'Mk Mk {0} : "{1}" U{2} # {3}'.format(
+#             ' '.join(keys), e, format(ord(e), 'x').upper(), unicodedata.name(e)))
+#     except Exception as e:
+#         continue
